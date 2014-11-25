a03c2135367e0fc7428ebebfba2e798354b05daa
Adam Rawlings <adam@adamcolinrawlings.com>
2014-11-25 05:03:27 -0500

    progress...

--- src/Acme/CounterBundle/Controller/CounterController.php
+++ src/Acme/CounterBundle/Controller/CounterController.php
@@ -2,10 +2,26 @@
 namespace Acme\CounterBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Acme\CounterBundle\Entity\Item;
+use Symfont\Component\HttpFoundation\Response;
 
 class CounterController extends Controller
 {
-	public function indexAction($item)
+	
+public function createAction()
+{
+	$item = new Item();
+	$item->setName($item);
+	$item->setNumber($number);
+	
+	$em = $this->getDoctrine()->getManager();
+	$em->persist($item);
+	$em->flush();
+
+
+}
+
+public function indexAction($item)
     {
         return $this->render('AcmeCounterBundle:Default:index.html.twig', array('item' => $item));
     }
